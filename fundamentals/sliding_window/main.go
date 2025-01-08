011243e0a68702094f5323983e4dd0108f3f06d0
Benjamin Dixon <5067649+bcdxn@users.noreply.github.com>
2025-01-08 13:42:33 -0500

    test

--- fundamentals/sliding_window/main.go
+++ fundamentals/sliding_window/main.go
@@ -9,25 +9,27 @@ func main() {
 }
 
 func maxContiguousSegment(nums []int, k int) int {
-	max := 0 // track maximum window sum
+	max := 0
 
-	if k > len(nums) {
-		for i := 0; i < len(nums); i++ {
-			max += nums[i]
+	if k >= len(nums) {
+		for _, val := range nums {
+			max += val
 		}
 		return max
 	}
 
+	// get sum in initial window
+	windowSum := 0
 	for i := 0; i < k; i++ {
-		max += nums[i]
+		windowSum += nums[i]
 	}
+	max = windowSum
 
-	sum := max // track current window
-
+	// slide the window
 	for i := 0; i < len(nums)-k; i++ {
-		sum = sum - nums[i] + nums[i+k]
-		if sum > max {
-			max = sum
+		windowSum = windowSum - nums[i] + nums[i+k]
+		if windowSum > max {
+			max = windowSum
 		}
 	}
 
