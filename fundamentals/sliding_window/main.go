e0ff33ea2181c44fd73fd89aad6bfca30c183b30
Benjamin Dixon <5067649+bcdxn@users.noreply.github.com>
2025-01-12 14:13:36 -0500

    polish sliding window

--- fundamentals/sliding_window/main.go
+++ fundamentals/sliding_window/main.go
@@ -4,28 +4,23 @@ import "fmt"
 
 func main() {
 	nums := []int{8, 3, -2, 4, 5, -1, 0, 5, 3, 9, -6}
-	res := maxContiguousSegment(nums, 5)
+	res := maxSumInWindow(nums, 5)
 	fmt.Println("max:", res) // 18
 }
 
-func maxContiguousSegment(nums []int, k int) int {
-	max := 0
-
-	if k >= len(nums) {
-		for _, val := range nums {
-			max += val
-		}
-		return max
+func maxSumInWindow(nums []int, k int) int {
+	if k > len(nums) {
+		return 0
 	}
-
-	// get sum in initial window
 	windowSum := 0
+
+	// sum initial window
 	for i := 0; i < k; i++ {
 		windowSum += nums[i]
 	}
-	max = windowSum
+	max := windowSum
 
-	// slide the window
+	// slide window and update sum
 	for i := 0; i < len(nums)-k; i++ {
 		windowSum = windowSum - nums[i] + nums[i+k]
 		if windowSum > max {
