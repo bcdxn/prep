8847f34968edfdf1b6bafb0947d39965e196b702
Benjamin Dixon <5067649+bcdxn@users.noreply.github.com>
2025-01-12 19:53:05 -0500

    feat: add sliding window of variable length example

---
+++ fundamentals/sliding_window/variable/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	t := "abcabcbb"
+
+	n := longestSubStr(t) // should print 3
+
+	fmt.Println("longest:", n)
+}
+
+func longestSubStr(s string) int {
+	charSet := make(map[byte]struct{})
+
+	max := 0 // max length of substring
+	l := 0   // start of window
+
+	for r := range len(s) {
+		_, ok := charSet[s[r]]
+		for ok {
+			delete(charSet, s[l])
+			l++
+			_, ok = charSet[s[r]]
+		}
+		charSet[s[r]] = struct{}{}
+		currWindowSize := r - l + 1
+		if currWindowSize > max {
+			max = currWindowSize
+		}
+	}
+
+	return max
+}
