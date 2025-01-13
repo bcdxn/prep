d067a58120bdb9e5848836d7410305d5a7b26d43
Benjamin Dixon <5067649+bcdxn@users.noreply.github.com>
2025-01-13 11:39:39 -0500

    chore: dfs quiz

--- fundamentals/algorithms/dfs_matrix/main.go
+++ fundamentals/algorithms/dfs_matrix/main.go
@@ -10,48 +10,53 @@ func main() {
 		{0, 1, 0, 0},
 	}
 
-	paths := dfs(m, 0, 0, make(map[string]struct{}))
-	fmt.Println("paths:", paths) // 2 paths
+	visited := make(map[rc]struct{})
+	paths := dfs(m, 0, 0, visited)
+	fmt.Println("num paths: ", paths)
 }
 
-func dfs(m [][]int, r int, c int, visited map[string]struct{}) int {
+type rc struct {
+	r int
+	c int
+}
+
+func dfs(m [][]int, r int, c int, visited map[rc]struct{}) int {
 	rows := len(m)
 	if rows < 1 {
 		return 0
 	}
+
 	cols := len(m[0])
 	if cols < 1 {
 		return 0
 	}
 
-	// out of bounds base case
+	// out of bounds
 	if r < 0 || r >= rows || c < 0 || c >= cols {
 		return 0
 	}
-	// blocked base case
+	// already visited
+	if _, ok := visited[rc{r, c}]; ok {
+		return 0
+	}
+	// blocked
 	if m[r][c] == 1 {
 		return 0
 	}
-	// already visited base case
-	visitKey := fmt.Sprintf("%d:%d", r, c)
-	if _, ok := visited[visitKey]; ok {
-		return 0
-	}
-	// reached the end base case
+	// reached destination
 	if r == rows-1 && c == cols-1 {
 		return 1
 	}
 
-	// visiting new node
-	visited[visitKey] = struct{}{}
 	count := 0
+	visited[rc{r, c}] = struct{}{}
 
-	count += dfs(m, r+1, c, visited)
 	count += dfs(m, r-1, c, visited)
-	count += dfs(m, r, c+1, visited)
+	count += dfs(m, r+1, c, visited)
 	count += dfs(m, r, c-1, visited)
+	count += dfs(m, r, c+1, visited)
 
-	delete(visited, visitKey)
+	delete(visited, rc{r, c})
 
 	return count
 }
