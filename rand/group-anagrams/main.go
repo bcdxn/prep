d807b2fd6b7bac01413df0c15b7c892f8093fbe3
Benjamin Dixon <5067649+bcdxn@users.noreply.github.com>
2025-01-26 14:04:42 -0500

    feat: make anagrams grouping more efficient

--- rand/group-anagrams/main.go
+++ rand/group-anagrams/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
-	"strconv"
 )
 
 func main() {
@@ -13,37 +11,27 @@ func main() {
 
 func groupAnagrams(words []string) [][]string {
 	anagramGroups := make([][]string, 0)
-	anagrams := make(map[string]int)
+	anagramsMap := make(map[[26]int]int)
+
+	// use an array as hashmap for ascii characters
+	// aaabc
+	// [3, 1, 1]
 
 	// loop given words
 	for _, word := range words {
-		currAnagram := make(map[rune]int)
+		currAnagramKey := [26]int{}
+		a := int('a')
 		// build anagram set
 		for _, ch := range word {
-			if count, ok := currAnagram[ch]; ok {
-				currAnagram[ch] = count + 1
-			} else {
-				currAnagram[ch] = 1
-			}
+			i := int(ch) - a
+			currAnagramKey[i] += 1
 		}
-		// sort characters
-		chars := make([]rune, 0, len(currAnagram))
-		for ch := range currAnagram {
-			chars = append(chars, ch)
-		}
-		sort.Slice(chars, func(i, j int) bool {
-			return chars[i] < chars[j]
-		})
-		// build a key from the anagram a2b3c1
-		key := ""
-		for _, ch := range chars {
-			key += string(ch) + strconv.Itoa(currAnagram[ch])
-		}
-		if index, ok := anagrams[key]; ok {
+
+		if index, ok := anagramsMap[currAnagramKey]; ok {
 			anagramGroups[index] = append(anagramGroups[index], word)
 		} else {
 			anagramGroups = append(anagramGroups, []string{word})
-			anagrams[key] = len(anagramGroups) - 1
+			anagramsMap[currAnagramKey] = len(anagramGroups) - 1
 		}
 	}
 
