5245337edf03518aa4f79ebaaadce383dc7ba3fc
Benjamin Dixon <5067649+bcdxn@users.noreply.github.com>
2025-02-10 15:28:17 -0500

    chore: heap polish

--- fundamentals/data_structures/heap.go
+++ fundamentals/data_structures/heap.go
@@ -11,6 +11,7 @@ type MinHeap[T constraints.Ordered] struct {
 	h []T
 }
 
+// Add an item to the heap and maintain heap properties.
 func (h *MinHeap[T]) Push(item T) {
 	if len(h.h) < 2 {
 		var zero T
@@ -18,52 +19,64 @@ func (h *MinHeap[T]) Push(item T) {
 		return
 	}
 
-	// add element at next index to keep *complete* tree
+	// add item to the end to maintain complete property
 	h.h = append(h.h, item)
 	// percolate up
 	i := len(h.h) - 1
-	for h.h[i] < h.h[i/2] {
-		h.h[i], h.h[i/2] = h.h[i/2], h.h[i]
-		i = i / 2
-	}
-}
-
-func (h *MinHeap[T]) Pop() (T, error) {
-	if len(h.h) < 2 {
-		var zero T
-		return zero, errors.New("cannot pop from empty heap")
-	}
-
-	if len(h.h) == 2 {
-		item := h.h[1]
-		h.h = []T{}
-		return item, nil
-	}
-
-	item := h.h[1]
-
-	// replace root with last element in heap
-	h.h[1] = h.h[len(h.h)-1]
-	h.h = h.h[:len(h.h)-1]
-	// percolate down
-	i := 1
-	for 2*i < len(h.h) {
-		if 2*i+1 < len(h.h) && h.h[i] > h.h[2*i+1] && h.h[2*i+1] < h.h[2*i] {
-			// has right child AND item > right child AND right child < left
-			h.h[i], h.h[2*i+1] = h.h[2*i+1], h.h[i]
-			i = 2*i + 1
-		} else if 2*i < len(h.h) && h.h[i] > h.h[2*i] {
-			// item is greater than left child
-			h.h[i], h.h[2*i] = h.h[2*i], h.h[i]
-			i = 2 * i
+	for {
+		if h.h[i] < h.h[parent(i)] {
+			h.h[i], h.h[parent(i)] = h.h[parent(i)], h.h[i]
+			i = parent(i)
 		} else {
 			break
 		}
 	}
+}
 
+// Remove the item from the top of the heap and maintain heap properties.
+func (h *MinHeap[T]) Pop() (T, error) {
+	if len(h.h) < 2 {
+		var zero T
+		return zero, errors.New("cannot perform pop on empty heap")
+	}
+	// take current top of heap
+	item := h.h[1]
+	// copy last item in heap to top and truncate last item
+	h.h[1] = h.h[len(h.h)-1]
+	h.h = h.h[:len(h.h)-1]
+	// percolate down
+	i := 1
+	for left(i) < len(h.h) {
+		// has a right child, right child < left child, item > right child
+		if right(i) < len(h.h) && h.h[right(i)] < h.h[left(i)] && h.h[i] > h.h[right(i)] {
+			h.h[i], h.h[right(i)] = h.h[right(i)], h.h[i]
+			i = right(i)
+		} else if h.h[i] > h.h[left(i)] {
+			h.h[i], h.h[left(i)] = h.h[left(i)], h.h[i]
+			i = left(i)
+		} else {
+			break
+		}
+	}
 	return item, nil
 }
 
+// Return a string representation of the heap.
 func (h *MinHeap[T]) String() string {
 	return fmt.Sprintf("%v", h.h)
+}
+
+// get the index into the backing slice of the parent of a given node index.
+func parent(i int) int {
+	return i / 2
+}
+
+// get index into the backing slice of the left child of a given node index.
+func left(i int) int {
+	return i * 2
+}
+
+// get the index into the backing slice of the right child of a given node index.
+func right(i int) int {
+	return i*2 + 1
 }
