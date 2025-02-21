d5a73b2946066f4b3e8b09def151cf2c00cae657
Benjamin Dixon <5067649+bcdxn@users.noreply.github.com>
2025-02-20 22:45:10 -0500

    bs

--- fundamentals/algorithms/binary_search/main.go
+++ fundamentals/algorithms/binary_search/main.go
@@ -4,24 +4,28 @@ import "fmt"
 
 func main() {
 	haystack := []int{1, 3, 3, 4, 5, 6, 7, 8}
-	needle := 6
-	i := binarySearch(haystack, needle)
-	fmt.Println("index found:", i)
+
+	index := bs(6, haystack)
+
+	fmt.Printf("found needle %d at index %d\n", 6, index)
 }
 
-func binarySearch(haystack []int, needle int) int {
-	l := 0
-	r := len(haystack) - 1
+func bs(needle int, haystack []int) int {
+	left := 0
+	right := len(haystack) - 1
 
-	for l <= r {
-		m := (l + r) / 2
+	for left <= right {
+		// mid := (left + right) / 2        // -- simplified expression to calculate midpoint
+		mid := left + (right-left)/2
 
-		if needle < haystack[m] {
-			r = m - 1
-		} else if needle > haystack[m] {
-			l = m + 1
+		if needle < haystack[mid] {
+			// search left half of haystack
+			right = mid - 1
+		} else if needle > haystack[mid] {
+			// search left half of haystack
+			left = mid + 1
 		} else {
-			return m
+			return mid
 		}
 	}
 
