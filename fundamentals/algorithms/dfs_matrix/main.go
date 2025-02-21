d64bda801b13bdc61134f76e769332018a67db53
Benjamin Dixon <5067649+bcdxn@users.noreply.github.com>
2025-02-21 14:55:34 -0500

    prac

--- fundamentals/algorithms/dfs_matrix/main.go
+++ fundamentals/algorithms/dfs_matrix/main.go
@@ -10,43 +10,37 @@ func main() {
 		{0, 1, 0, 0},
 	}
 
-	paths := dfs(matrix, 0, 0, make(map[rc]struct{}))
-	fmt.Println("paths: ", paths)
+	visited := map[rc]struct{}{}
+	numPaths := dfs(matrix, 0, 0, visited)
+
+	fmt.Println("numPaths: ", numPaths)
 }
 
-func dfs(m [][]int, r int, c int, visited map[rc]struct{}) int {
+func dfs(m [][]int, r, c int, visited map[rc]struct{}) int {
 	rows := len(m)
 	cols := len(m[0])
 
+	// at the destination
 	if r == rows-1 && c == cols-1 {
 		return 1
 	}
 
+	// already visited, out of bounds, or blocked
+	if _, ok := visited[rc{r, c}]; ok || r < 0 || r >= rows || c < 0 || c >= cols || m[r][c] == 1 {
+		return 0
+	}
+
+	// none of the above; mark the current node visited and keep recursing
+	visited[rc{r, c}] = struct{}{}
 	count := 0
 
-	if _, ok := visited[rc{r - 1, c}]; !ok && r-1 >= 0 && m[r-1][c] != 1 {
-		visited[rc{r - 1, c}] = struct{}{}
-		count += dfs(m, r-1, c, visited)
-		delete(visited, rc{r - 1, c})
-	}
+	count += dfs(m, r-1, c, visited)
+	count += dfs(m, r, c+1, visited)
+	count += dfs(m, r+1, c, visited)
+	count += dfs(m, r, c-1, visited)
 
-	if _, ok := visited[rc{r + 1, c}]; !ok && r+1 < rows && m[r+1][c] != 1 {
-		visited[rc{r + 1, c}] = struct{}{}
-		count += dfs(m, r+1, c, visited)
-		delete(visited, rc{r + 1, c})
-	}
-
-	if _, ok := visited[rc{r, c - 1}]; !ok && c-1 >= 0 && m[r][c-1] != 1 {
-		visited[rc{r, c - 1}] = struct{}{}
-		count += dfs(m, r, c-1, visited)
-		delete(visited, rc{r, c - 1})
-	}
-
-	if _, ok := visited[rc{r, c + 1}]; !ok && c+1 < cols && m[r][c+1] != 1 {
-		visited[rc{r, c + 1}] = struct{}{}
-		count += dfs(m, r, c+1, visited)
-		delete(visited, rc{r, c + 1})
-	}
+	// unvisit the current node so that it can be used on other distinct paths
+	delete(visited, rc{r, c})
 
 	return count
 }
