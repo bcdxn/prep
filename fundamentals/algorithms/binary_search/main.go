dd614c818e25c1fb9ba35eadceb1116157036eb0
bcdxn <5067649+bcdxn@users.noreply.github.com>
2025-04-09 21:20:40 -0400

    prac

--- fundamentals/algorithms/binary_search/main.go
+++ fundamentals/algorithms/binary_search/main.go
@@ -5,27 +5,35 @@ import "fmt"
 func main() {
 	haystack := []int{1, 3, 3, 4, 5, 6, 7, 8}
 
-	index := bs(6, haystack)
-
-	fmt.Printf("found needle %d at index %d\n", 6, index)
+	i := find(3, haystack)
+	fmt.Println("found", i)
+	i = find(4, haystack)
+	fmt.Println("found", i)
+	i = find(8, haystack)
+	fmt.Println("found", i)
+	i = find(1, haystack)
+	fmt.Println("found", i)
+	i = find(5, haystack)
+	fmt.Println("found", i)
+	i = find(10, haystack)
+	fmt.Println("found", i)
+	i = find(2, haystack)
+	fmt.Println("found", i)
 }
 
-func bs(needle int, haystack []int) int {
-	left := 0
-	right := len(haystack) - 1
+func find(needle int, haystack []int) int {
+	l := 0
+	r := len(haystack) - 1
 
-	for left <= right {
-		// mid := (left + right) / 2        // -- simplified expression to calculate midpoint
-		mid := left + (right-left)/2
+	for l <= r {
+		m := l + ((r - l) / 2)
 
-		if needle < haystack[mid] {
-			// search left half of haystack
-			right = mid - 1
-		} else if needle > haystack[mid] {
-			// search left half of haystack
-			left = mid + 1
-		} else {
-			return mid
+		if needle == haystack[m] {
+			return m
+		} else if needle < haystack[m] {
+			r = m - 1
+		} else if needle > haystack[m] {
+			l = m + 1
 		}
 	}
 
